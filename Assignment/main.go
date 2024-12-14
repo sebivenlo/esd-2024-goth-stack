67c621f8ce79f142495b8692dadad39fb9555415
Joel Eckhardt <495223@student.fontys.nl>
2024-12-14 21:29:03 +0100

    Finished assignment 1-6

--- Assignment/main.go
+++ Assignment/main.go
@@ -1,14 +1,44 @@
 package main
- 
-import (
-    "net/http"
-    "workshop/views"
- 
-    "github.com/a-h/templ"
-)
- 
-func main() {
-    http.Handle("/", templ.Handler(views.Index()))
 
-    http.ListenAndServe("localhost:8080", nil)
+import (
+	"net/http"
+	"workshop/views"
+
+	"github.com/a-h/templ"
+)
+
+var todos = []views.Todo{
+	{Id: 1, Title: "Buy milk", Completed: false},
+	{Id: 2, Title: "Buy eggs", Completed: true},
+}
+
+func main() {
+	http.Handle("/", templ.Handler(views.Index()))
+	http.HandleFunc("/todos", handleTodos)
+	http.HandleFunc("/todos/", handleTodo)
+
+	http.ListenAndServe("localhost:8080", nil)
+}
+
+func handleTodos(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		views.TodoList(todos).Render(r.Context(), w)
+	case http.MethodPost:
+		title := r.FormValue("title")
+		newTodo := views.Todo{
+			Id:        len(todos) + 1,
+			Title:     title,
+			Completed: false,
+		}
+		todos = append([]views.Todo{newTodo}, todos...)
+		views.TodoItemOob(newTodo, "afterbegin").Render(r.Context(), w)
+	}
+}
+
+func handleTodo(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodDelete:
+		w.Write([]byte(""))
+	}
 }
