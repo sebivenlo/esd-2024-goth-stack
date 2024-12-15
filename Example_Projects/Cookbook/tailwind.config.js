297958a080329cd2b4b99a3516aa3b29ade3ab36
MelleDanski <m.bogdanski@student.fontys.nl>
2024-12-15 04:36:18 +0100

    Updated index, recipe_list and img

---
+++ Example_Projects/Cookbook/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    content: ["./views/**/*.{html,js,templ}"],
+    theme: {
+        extend: {
+            colors: {
+                beige: "#f5f5dc",
+                cream: "#fffdd0",
+                softPink: "#f8e1e7",
+                copper: "#b87333",
+                berry: "#B6244F",
+                darkGreen: "#1d4319",
+                lightGreen: "#819a47",
+                warmBrown: "#B89685",
+                darkBrown: "#504746",
+                santaRed: "#af271c",
+                berryTwo: "#5e1c25",
+                puddingBrown: "#320f12"
+
+
+            },
+            fontFamily: {
+                serif: ["Georgia", "serif"],
+                sans: ["Lato", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
